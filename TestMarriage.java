0f653073c881407e5286959dbb8d77479a85e1c2
KeithAlaan <keithalaan7@gmail.com>
2025-11-28 19:24:29 +0700

    Update TestMarriage.java

--- TestMarriage.java
+++ TestMarriage.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class TestMarriage 
 {
    public static void main(String[] args)
    {
       //for academic purposes only, no hate intended, love love love
-      Person kenneth = new Person("Egos", "Kenneth", "P", 'M');
-      Person kathryn = new Person("Bernardo", "Kathryn", "", 'F');
+      Person kenneth = new Person("Egos", "Kenneth", "P", 'M', 22);
+      Person kathryn = new Person("Bernardo", "Kathryn", "", 'F', 21);
       kenneth.displayDetails();
       kathryn.displayDetails();
       
@@ -14,7 +16,7 @@ public class TestMarriage
       }
       else 
       {
-         System.out.println("Sorry, not compatible. No hate intended, love love love.");
+         System.out.println("Sorry, not compatible. No hate intended, Minor love love love.");
       }
       
       kathryn.displayDetails();
